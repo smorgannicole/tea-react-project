54ae138414b79e3cd59bb5cfc23b50f398293303
Morgan <smorgannicole@gmail.com>
2023-12-15 19:43:13 -0600

    woohoo! got tabs functioning

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,36 @@
-import MenuItem from "./components/MenuItem";
 import headerLogo from "../Header/components/images/misty-tea-logo.png"
+import { useState } from "react";
 
 const Header = () => {
+    const [toggleState, setToggleState] = useState(1);
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
     return (
         <header className='container-fluid'>
             <div className="row">
                 <div className='col-3'>
                     <img className="header-logo" src={headerLogo} />
                 </div>
-                <nav className='col d-flex align-items-center justify-content-end'>
-                    <ul className='d-flex list-unstyled align-items-end gap-3'>
-                        <MenuItem text="Tea Blends" link="#" />
-                        <MenuItem text="Create Your Own" link="#" />
-                        <MenuItem text="About Us" link="#" />
-                    </ul>
+                <nav className='col d-flex align-items-center justify-content-end block-tabs'>
+                    <div className={toggleState === 1 ? "tab active-tab" : "tab"} onClick={() => toggleTab(1)}>Tea Blends</div>
+                    <div className={toggleState === 2 ? "tab active-tab" : "tab"} onClick={() => toggleTab(2)}>Create Your Own</div>
+                    <div className={toggleState === 3 ? "tab active-tab" : "tab"} onClick={() => toggleTab(3)}>About Us</div>
                 </nav>
+            </div>
+            <div className="content-tabs">
+                <div className={toggleState === 1 ? "content active-content" : "content"}>
+                    <h2>Tea Blends</h2>
+                    <p>lorem</p>
+                </div>
+                <div className={toggleState === 2 ? "content active-content" : "content"}>
+                    <h2>Create Your Own</h2>
+                    <p>lorem</p>
+                </div>
+                <div className={toggleState === 3 ? "content active-content" : "content"}>
+                    <h2>About Us</h2>
+                    <p>lorem</p>
+                </div>
             </div>
         </header>
     );
--- src/components/HeroWrapper/FavoriteTeasContainer/index.js
+++ src/components/HeroWrapper/FavoriteTeasContainer/index.js
@@ -2,7 +2,7 @@ import FavoriteTeas from "./FavoriteTeas";
 
 const FavoriteTeasContainer = () => {
     return (
-        <div className="col">
+        <div className="col favTeasCol">
             <FavoriteTeas/>
         </div>
     );
