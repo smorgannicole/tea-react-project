dbb45478b2a3d5cbe5d5fe5c432831ac925774e3
Morgan <smorgannicole@gmail.com>
2023-12-17 16:23:41 -0600

    radio button functionality

--- src/components/Main/components/FavoriteTeasContainer/FavoriteTeas/index.js
+++ src/components/Main/components/FavoriteTeasContainer/FavoriteTeas/index.js
@@ -5,8 +5,10 @@ import whiteTeaImg from "../../images/White-tea.png"
 import oolongImg from "../../images/oolong.png"
 import earlGreyImg from "../../images/earl-grey.png"
 import englishImg from "../../images/english-breakfast.png"
+import { useState } from "react";
 
 const FavoriteTeas = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
     const favoriteTeas = [
         {
             name: "Matcha Tea",
@@ -33,11 +35,45 @@ const FavoriteTeas = () => {
             image: <img className="fav-tea" src={englishImg} alt="tea" />,
         },
     ];
+    const updateIndex = (newIndex) => {
+        if (newIndex < 0) {
+            newIndex = 0;
+        } else if (newIndex > favoriteTeas.length) {
+            newIndex = favoriteTeas.length - 1;
+        }
+        setActiveIndex(newIndex);
+    }
 //carousel-item next to col
     return (
         <div className="col">
-            {favoriteTeas.map((favoriteTea) => (
-                <FavoriteTea text={favoriteTea.name} img={favoriteTea.image} />))}
+            <div className="inner"
+                    style={{transform: `translate:(-${activeIndex * 100}%)`}}>
+                {favoriteTeas.map((favoriteTea) => (
+                    <FavoriteTea text={favoriteTea.name} img={favoriteTea.image} />))}
+            </div>
+            <div className="carousel-buttons">
+                <button onClick={()=> {
+                    updateIndex(activeIndex - 1);
+                }} className="button-arrow">
+                    <span class="material-symbols-outlined">arrow_back_ios</span>
+                </button>
+                <div className="indicators">
+                    {favoriteTeas.map((favoriteTea, index) => {
+                        return (
+                            <button onClick={()=> {
+                                updateIndex(index);
+                            }} className="indicator-buttons">
+                                <span className={`material-symbols-outlined ${index === activeIndex? "indicator-symbol-active" : "indicator-symbol"}`}>radio_button_checked</span>
+                            </button>
+                        );
+                    })}
+                </div>
+                <button onClick={()=> {
+                    updateIndex(activeIndex + 1);
+                }} className="button-arrow">
+                    <span class="material-symbols-outlined">arrow_forward_ios</span>
+                </button>
+            </div>
         </div>
     );
 }
