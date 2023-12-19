c83155ee3ead44985438b18159bc46962e1e95cd
Morgan <smorgannicole@gmail.com>
2023-12-19 10:05:39 -0600

    added social media icons

--- src/components/Main/components/FavoriteTeasContainer/FavoriteTeas/index.js
+++ src/components/Main/components/FavoriteTeasContainer/FavoriteTeas/index.js
@@ -5,6 +5,10 @@ import whiteTeaImg from "../../images/White-tea.png"
 import oolongImg from "../../images/oolong.png"
 import earlGreyImg from "../../images/earl-grey.png"
 import englishImg from "../../images/english-breakfast.png"
+import instaIcon from "../../images/insta-icon.png"
+import xIcon from "../../images/twitter-icon.png"
+import facebookIcon from "../../images/facebook-icon.png"
+import youtubeIcon from "../../images/youtube.icon.png"
 import { useState } from "react";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
@@ -76,8 +80,13 @@ const FavoriteTeas = () => {
                 </button>
             </div>
             <div>
-                <h3 style={{color:"white", fontWeight: "lighter"}}>socials</h3>
-
+                <hr style={{color:"white"}}></hr>
+                <div className="row">
+                    <div className="col"><img src={instaIcon} className="social-icons" alt="instagram icon"/></div>
+                    <div className="col"><img src={xIcon} className="social-icons" alt="x icon"/></div>
+                    <div className="col"><img src={facebookIcon} className="social-icons" alt="facebook icon"/></div>
+                    <div className="col"><img src={youtubeIcon} className="social-icons" alt="youtube icon"/></div>
+                </div>
             </div>
         </div>
     );
